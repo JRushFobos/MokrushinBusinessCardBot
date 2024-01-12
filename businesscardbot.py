5f7efda760186c1e24d291b642cdfe0eb9175e1d
JRushFobos <fobos_media@mail.ru>
2024-01-12 23:43:40 +0500

    add buttons and info cv bot

--- businesscardbot.py
+++ businesscardbot.py
@@ -1,13 +1,13 @@
 import os
 import logging
 
-import get_info
 import telegram
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, Updater, CallbackQueryHandler
 
 from dotenv import load_dotenv
 
-from menu import (main_buttons,)
+from menu import main_buttons
+import text
 
 
 logging.basicConfig(
@@ -36,16 +36,51 @@ def wake_up(update, context):
 
 def main_menu(update, context):
     chat = update.effective_chat
-    name = update.message.chat.first_name
 
     buttons = main_buttons
     keyboard = telegram.InlineKeyboardMarkup(buttons)
 
     context.bot.send_message(
         chat_id=chat.id,
-        text="Выбери категорию.",
+        text="Выберите интересующую вас категорию:",
         reply_markup=keyboard,
     )
+
+def about_me(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, text=text.about_me)
+    main_menu(update, context)
+
+def skills(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, text=text.skills)
+    main_menu(update, context)
+
+def cv_link(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, text=text.cv_link)
+    main_menu(update, context)
+
+def cv_pdf(update, context):
+    chat = update.effective_chat
+    pdf_file = 'cv.pdf'
+    context.bot.send_document(chat.id, document=open(pdf_file, 'rb'))
+    main_menu(update, context)
+
+def work_experience(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, text=text.work_experience)
+    main_menu(update, context)
+
+def myprojects(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, text=text.myprojects)
+    main_menu(update, context)
+
+def mycontacts(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, text=text.mycontacts)
+    main_menu(update, context)
 
 # Очищаем данные
 def reset_conversation(update, context):
@@ -57,6 +92,13 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', wake_up))
+    dp.add_handler(CallbackQueryHandler(about_me, pattern='aboutme'))
+    dp.add_handler(CallbackQueryHandler(skills, pattern='skills'))
+    dp.add_handler(CallbackQueryHandler(cv_link, pattern='cv_link'))
+    dp.add_handler(CallbackQueryHandler(cv_pdf, pattern='cv_pdf'))
+    dp.add_handler(CallbackQueryHandler(work_experience, pattern='experiance'))
+    dp.add_handler(CallbackQueryHandler(myprojects, pattern='myprojects'))
+    dp.add_handler(CallbackQueryHandler(mycontacts, pattern='mycontacts'))
 
     updater.start_polling()
     updater.idle()
--- menu.py
+++ menu.py
@@ -3,14 +3,17 @@ from telegram import InlineKeyboardButton
 main_buttons = [
     [
         InlineKeyboardButton("🕵️‍♀️ Обо мне", callback_data="aboutme"),
-        InlineKeyboardButton("📁 Резюме", callback_data="cv"),
+        InlineKeyboardButton("👩‍🎓 Навыки", callback_data="skills"),
     ],
     [
-        InlineKeyboardButton("👩‍🎓 Навыки", callback_data="hardskills"),
-        InlineKeyboardButton("💼 Мои проекты", callback_data="myprojects"),
+        InlineKeyboardButton("👆 Резюме(Link)", callback_data="cv_link"),
+        InlineKeyboardButton("📁 Резюме(PDF)", callback_data="cv_pdf"),
     ],
     [
         InlineKeyboardButton("⛏ Опыт", callback_data="experiance"),
-        InlineKeyboardButton("🧒 Фотографии", callback_data="photo"),
+        InlineKeyboardButton("💼 Мои проекты", callback_data="myprojects"),
     ],
+    [
+        InlineKeyboardButton("☎️ Мои контакты", callback_data="mycontacts"),
+    ]
 ]
